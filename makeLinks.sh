cc6a0a1d55c0ae6c87732e9932659be8d0859fa6
viyoriya <viyoriya@gmail.com>
2021-06-11 00:46:30 +0100

    dmenu timer and function added

--- makeLinks.sh
+++ makeLinks.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dbookmark /usr/local/bin/d_bookmark
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dfm /usr/local/bin/d_fm
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dmount /usr/local/bin/d_mount
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dpass /usr/local/bin/d_pass
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/drecord /usr/local/bin/d_record
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dsearch /usr/local/bin/d_search
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dtodo /usr/local/bin/d_todo
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dtimer /usr/local/bin/d_timer 
-sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dlist /usr/local/bin/dls 
+dirPath=~/Documents/GIT_PROJECTS/dmenuScripts
+
+sudo ln -s $dirPath/dbookmark /usr/local/bin/d_bookmark
+sudo ln -s $dirPath/dfm /usr/local/bin/d_fm
+sudo ln -s $dirPath/dmount /usr/local/bin/d_mount
+sudo ln -s $dirPath/dpass /usr/local/bin/d_pass
+sudo ln -s $dirPath/drecord /usr/local/bin/d_record
+sudo ln -s $dirPath/dsearch /usr/local/bin/d_search
+sudo ln -s $dirPath/dtodo /usr/local/bin/d_todo
+sudo ln -s $dirPath/dtimer /usr/local/bin/d_timer 
+sudo ln -s $dirPath/dlist /usr/local/bin/dls 
+cp $dirPath/etc/*.* ~/.dwm/
+cp $dirPath/etc/*.* ~/.config/dwm/
 
 #sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dcrypto d_crypto
 #sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dmount_ssd d_mount_ssd 
