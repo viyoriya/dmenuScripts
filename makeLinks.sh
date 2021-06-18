be673e37963475b0b64a8fd4abd705112702524d
viyoriya <viyoriya@gmail.com>
2021-06-18 09:22:01 +0100

    Updated -18-06-2021

--- makeLinks.sh
+++ makeLinks.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
+#directory path
 dirPath=~/Documents/GIT_PROJECTS/dmenuScripts
+
+[ ! -d "$dirPath" ] && echo "Change the directory path"
+
 
 sudo ln -s $dirPath/dbookmark /usr/local/bin/d_bookmark
 sudo ln -s $dirPath/dfm /usr/local/bin/d_fm
@@ -11,8 +16,9 @@ sudo ln -s $dirPath/dsearch /usr/local/bin/d_search
 sudo ln -s $dirPath/dtodo /usr/local/bin/d_todo
 sudo ln -s $dirPath/dtimer /usr/local/bin/d_timer 
 sudo ln -s $dirPath/dlist /usr/local/bin/dls 
-cp $dirPath/etc/*.* ~/.dwm/
-cp $dirPath/etc/*.* ~/.config/dwm/
 
-#sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dcrypto d_crypto
-#sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dmount_ssd d_mount_ssd 
+[ -d "~/.config/dwm" ] && cp $dirPath/etc/*.* ~/.config/dwm/
+[ -d "~/.dwm" ] && cp $dirPath/etc/*.* ~/.dwm/
+
+#sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dcrypto /usr/local/bin/d_crypto
+#sudo ln -s /home/vj/Documents/GIT_PROJECTS/dmenuScripts/dmount_ssd /usr/local/bin/d_mount_ssd 
