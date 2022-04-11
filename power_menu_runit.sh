637466465e5bad736d9e65dc698b20ee6898e8e9
viyoriya <viyoriya@gmail.com>
2022-04-11 16:41:38 +0000

    Updated 11-04-2022

---
+++ power_menu_runit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#viyoriya 
+
+#DMENU_OPT=" -i -nb #2E3440 -nf #929496 -sb #929496 -sf #2E3440 -fn Iosevka:style=Regular:size=10"    
+#DMENU="dmenu -p   $DMENU_OPT"
+DMENU="dmenu -p   "
+
+SESSON_ID=$(loginctl session-status | awk 'NR==1{print $1}')
+Lock_command="slock"
+Exit_command="loginctl terminate-session $SESSON_ID"
+Reboot_command="sudo reboot"
+Shutdown_command="sudo poweroff"
+
+options=$' Lock\n Exit\n Reboot\n Shutdown' 
+eval \$"$(echo "$options" | $DMENU | awk '{print $2}')_command"
